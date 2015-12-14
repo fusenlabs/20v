f633a842416ca7e5ef7e1f586b267a8f94cebeee
Cosmitar <mhherrera31@gmail.com>
2015-12-14 00:52:36 -0300

    add scripts to deploy on gh-pages and remove /dist from git cache

--- bin/deploy.js
+++ bin/deploy.js
@@ -2,5 +2,21 @@
 
 var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { console.log(stdout); }
-exec('cd ./page', puts);
-exec('git status', puts);
+var copyFilesCommands = [
+    'cp ./index.html ./page/',
+    //'cp -r ./style/fonts/ ./page/',
+    //'cp ./style/style.css ./page/style/',
+    'rm -rf ./page/static',
+    'cp -r ./dist/ ./page/static/',
+].join(' && ');
+
+var gitCommands = [
+    'cd ./page',
+    'git add -A',
+    'git commit -a -m \'gh-pages update\'',
+    'git push origin gh-pages --force'
+].join(' && ');
+
+exec(copyFilesCommands, puts);
+//exec('cd ./page && git status', puts);
+exec(gitCommands, puts);
