b9a2e1ccdaa77cce8ff4d5c0a45a6dc6720924eb
Cosmitar <mhherrera31@gmail.com>
2016-01-13 01:24:47 -0300

    create static file CNAME instead of dynamic creation on deploy

--- bin/gh-deploy.js
+++ bin/gh-deploy.js
@@ -4,9 +4,6 @@ var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { console.log(stdout); }
 var copyFilesCommands = [
     'cp -R ./public/* ./page/',
-    'echo "www.20v.co" > ./page/CNAME',
-    //'cp -r ./style/fonts/ ./page/',
-    //'cp ./style/style.css ./page/style/',
     'rm -rf ./page/static',
     'cp -r ./dist/ ./page/static/',
 ].join(' && ');
