6db0a347ce314bb1f79572c5a47ad8cb50a2fc99
Joel <joelalovera@gmail.com>
2016-01-14 23:28:15 -0300

    fix conventions

--- src/components/share/index.js
+++ src/components/share/index.js
@@ -11,16 +11,18 @@ class Share extends Component {
             );
     }
 
-    _tw() {
+    _fb() {
         let url = `http://facebook.com/sharer.php?s=100&p[url]=http://www.20v.co`;
-        return (<a href='#' onClick={this._share.bind(this, url)}>
+        let share = this._share.bind(this, url);
+        return (<a href='#' onClick={share}>
                     <img src='facebook.svg'/>
                 </a>);
     }
 
-    _fb() {
+    _tw() {
         let url = `https://twitter.com/intent/tweet?text=text&url=http://www.20v.co&hashtags=music`;
-        return (<a href='#' onClick={this._share.bind(this, url)}>
+        let share = this._share.bind(this, url);
+        return (<a href='#' onClick={share}>
                     <img src='twitter.svg'/>
                 </a>);
     }
