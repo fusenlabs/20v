fd22814eabb2ed6d3a43090c451b76529c07cb97
Cosmitar <mhherrera31@gmail.com>
2016-01-16 02:50:35 -0300

    config devel and prod of webpack to export css on independant file

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,46 +1,7 @@
-/**
- * WEBPACK CONFIG
- *
- * Notes on config properties:
- *
- * 'entry'
- * Entry point for the bundle.
- *
- * 'output'
- * If you pass an array - the modules are loaded on startup. The last one is exported.
- *
- * 'resolve'
- * Array of file extensions used to resolve modules.
- *
- * devtool: 'eval-source-map'
- * http://www.cnblogs.com/Answer1215/p/4312265.html
- * The source map file will only be downloaded if you have source maps enabled and your dev tools open.
- *
- * OccurrenceOrderPlugin
- * Assign the module and chunk ids by occurrence count. Ids that are used often get lower (shorter) ids.
- * This make ids predictable, reduces to total file size and is recommended.
- *
- * UglifyJsPlugin
- * Minimize all JavaScript output of chunks. Loaders are switched into minimizing mode.
- *    - 'compress'
- *      Compressor is a tree transformer which reduces the code size by applying various optimizations on the AST.
- *
- * 'NODE_ENV'
- * React relies on process.env.NODE_ENV based optimizations.
- * If we force it to production, React will get in an optimized manner.
- * This will disable some checks (eg. property type checks) and give you a smaller build and improved performance.
- *    Note: That JSON.stringify is needed as webpack will perform string replace "as is".
- *    In this case we'll want to end up with strings as that's what various comparisons expect, not just production.
- *    Latter would just cause an error.
- *
- * 'babel'
- * Babel enables the use of ES6 today by transpiling your ES6 JavaScript into equivalent ES5 source
- * that is actually delivered to the end user browser.
- */
-
 /* eslint-disable no-var */
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/index',
@@ -64,6 +25,9 @@ module.exports = {
       compress: {
         warnings: false
       }
+    }),
+    new ExtractTextPlugin('style.css', {
+        allChunks: true
     })
   ],
   module: {
@@ -75,7 +39,10 @@ module.exports = {
       },
       {
           test: /\.scss$/,
-          loader: 'style!css!sass'
+          loader: ExtractTextPlugin.extract(
+              "style",
+              "css?minimize!sass"
+          )
       },
       {
           test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$/,
