53c162040fd06287604da4e7f75a48c3eeebe911
Joel <joelalovera@gmail.com>
2016-01-20 21:42:02 -0300

    test polifyll

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,10 @@ module.exports = {
             allChunks: true
         }),
         new webpack.ProvidePlugin({
-            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch',
+            'Map': 'core-js/fn/map',
+            'Symbol': 'core-js/fn/symbol',
+            'Promise': 'core-js/fn/promise'
         })
     ],
     module: {
